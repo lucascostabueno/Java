ec7705cf224fde2bda1a06a86b36bd6543949ad2
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-09 10:18:59 -0300

    secao 4: / aula 25: Processamento de dados em Java, Casting

---
+++ nelio-alves/teste-processamento/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exemplo 1");
+        int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println(x);
+        System.out.println(y);
+
+        System.out.println("Exemplo 2");
+        int a;
+        double b;
+        a = 5;
+        b = 2 * a;
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("Exemplo 3");
+        double c, B, h, area1;
+        c = 6.0;
+        B = 8.0;
+        h = 5.0;
+        area1 = (c + B) / 2.0 * h;
+        System.out.println(area1);
+
+        System.out.println("Exemplo 4");
+        float d, D, e, area;
+        d = 6f;
+        D = 8f;
+        e = 5f;
+        area = (d + D) / 2f * e;
+        System.out.println(area);
+
+        System.out.println("Exemplo 5");
+        int f, g;
+        double resultado1;
+        f = 5;
+        g = 2;
+        resultado1 = f / g;
+        System.out.println(resultado1);
+
+        System.out.println("Forma correta");
+        int j, i;
+        double resultado2;
+        j = 5;
+        i = 2;
+        resultado2 = (double) j / i;
+        System.out.println(resultado2);
+
+        System.out.println("Exemplo 6");
+        double k;
+        int l;
+        k = 5.0;
+        l = (int) k;
+        System.out.println(l);
+    }
+}
