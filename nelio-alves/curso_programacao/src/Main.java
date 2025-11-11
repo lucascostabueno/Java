00b0ba7d66e43f3a77a2bb8c4d36a305a0f7a102
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-10 21:02:32 -0300

    secao 4 / aula 27: Entrada de dados em Java - Parte 2

--- nelio-alves/curso_programacao/src/Main.java
+++ nelio-alves/curso_programacao/src/Main.java
@@ -1,43 +1,20 @@
-import java.util.Locale;
 import java.util.Scanner;
-
 public class Main {
-    static void main(String[] args) {
-        Locale.setDefault(Locale.US);
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Para ler uma palavra (texto sem espaços)
-        String a;
-        a = sc.next();
-        System.out.println("Voce digitou " + a);
-
-        // Para ler um número inteiro
-        int b;
-        b = sc.nextInt();
-        System.out.println("Voce digitou " + b);
-
-        // Para ler um número com ponto flutuante
-        double c;
-        c = sc.nextDouble();
-        System.out.println("Voce digitou " + c);
-
-        // Para ler um caractere
-        char d;
-        d = sc.next().charAt(0);
-        System.out.println("Voce digitou " + d);
-
-        // Para ler vários dados na mesma linha
-        String e;
-        int f;
-        double g;
-        e = sc.next();
-        f = sc.nextInt();
-        g = sc.nextDouble();
-        System.out.println("Dados digitados: ");
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-
+        int x;
+        String s1, s2, s3;
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        System.out.println("DADOS DIGITADOS:");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
         sc.close();
     }
 }
