6b1b4bba8e16ac6bb1250132c43d31a451d22d74
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-11 21:59:38 -0300

    secao 5 / aula 38: Sintaxe opcional - operadores de atribuição cumulativa

--- nelio-alves/curso_programacao/src/Main.java
+++ nelio-alves/curso_programacao/src/Main.java
@@ -1,43 +1,18 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Sintaxe da estrutura condicional simples
-        int a = 5;
-
-        System.out.println("Bom dia");
-
-        if (a < 0) {
-            System.out.println("Boa tarde");
-        }
-
-        System.out.println("Boa noite");
-
-        // Sintaxe da estrutura condicional composta
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int hora1;
 
-        System.out.println("Quantas horas ?");
-        hora1 = sc.nextInt();
+        int minutos = sc.nextInt();
 
-        if (hora1 < 12) {
-            System.out.println("Bom dia");
-        } else {
-            System.out.println("Boa tarde");
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0;
         }
 
-        // Sintaxe da estrutura condicional aninhadas
-        int hora2;
-
-        System.out.println("Quantas horas ?");
-        hora2 = sc.nextInt();
-
-        if (hora2 < 12) {
-            System.out.println("Bom dia");
-        } else if (hora2 < 18) {
-            System.out.println("Boa tarde");
-        } else {
-            System.out.println("Boa noite");
-        }
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
 
         sc.close();
     }
