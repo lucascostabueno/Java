a0b2acf6a347f0ad2260c1bd78fb22228320121d
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-11 21:21:46 -0300

    secao 5 / aula 35: Estrutura condicional (if-else)

--- nelio-alves/curso_programacao/src/Main.java
+++ nelio-alves/curso_programacao/src/Main.java
@@ -1,20 +1,44 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Sintaxe da estrutura condicional simples
+        int a = 5;
 
-        int x;
-        String s1, s2, s3;
-        x = sc.nextInt();
-        sc.nextLine();
-        s1 = sc.nextLine();
-        s2 = sc.nextLine();
-        s3 = sc.nextLine();
-        System.out.println("DADOS DIGITADOS:");
-        System.out.println(x);
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
+        System.out.println("Bom dia");
+
+        if (a < 0) {
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Boa noite");
+
+        // Sintaxe da estrutura condicional composta
+        Scanner sc = new Scanner(System.in);
+        int hora1;
+
+        System.out.println("Quantas horas ?");
+        hora1 = sc.nextInt();
+
+        if (hora1 < 12) {
+            System.out.println("Bom dia");
+        } else {
+            System.out.println("Boa tarde");
+        }
+
+        // Sintaxe da estrutura condicional aninhadas
+        int hora2;
+
+        System.out.println("Quantas horas ?");
+        hora2 = sc.nextInt();
+
+        if (hora2 < 12) {
+            System.out.println("Bom dia");
+        } else if (hora2 < 18) {
+            System.out.println("Boa tarde");
+        } else {
+            System.out.println("Boa noite");
+        }
+
         sc.close();
     }
 }
