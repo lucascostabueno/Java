0c922ef068b2ec3a46cf108b4022a1d0404daba7
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-13 21:34:37 -0300

    secao 5 / aula 40: pressão condicional ternária

--- nelio-alves/curso_programacao/src/Main.java
+++ nelio-alves/curso_programacao/src/Main.java
@@ -1,66 +1,18 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        /*Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        String dia;
-        if (x == 1) {
-            dia = "domingo";
-        }
-        else if (x == 2) {
-            dia = "segunda";
-        }
-        else if (x == 3) {
-            dia = "terca";
-        }
-        else if (x == 4) {
-            dia = "quarta";
-        }
-        else if (x == 5) {
-            dia = "quinta";
-        }
-        else if (x == 6) {
-            dia = "sexta";
-        }
-        else if (x == 7) {
-            dia = "sabado";
+        /*double preco = 34.5;
+        double desconto;
+        if (preco < 20.0) {
+            desconto = preco * 0.1;
         }
         else {
-            dia = "valor invalido";
-        }
-        System.out.println("Dia da semana: " + dia);
-        sc.close();*/
+            desconto = preco * 0.05;
+        }*/
 
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        String dia;
-        switch (x) {
-            case 1:
-                dia = "domingo";
-                break;
-            case 2:
-                dia = "segunda";
-                break;
-            case 3:
-                dia = "terca";
-                break;
-            case 4:
-                dia = "quarta";
-                break;
-            case 5:
-                dia = "quinta";
-                break;
-            case 6:
-                dia = "sexta";
-                break;
-            case 7:
-                dia = "sabado";
-                break;
-            default:
-                dia = "valor invalido";
-                break;
-        }
-        System.out.println("Dia da semana: " + dia);
-        sc.close();
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
     }
 }
