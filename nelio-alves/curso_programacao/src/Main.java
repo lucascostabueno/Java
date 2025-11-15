46055bdd7a704b94cd2e88f99f81fa982e9ac4bf
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-15 19:59:40 -0300

    secao 6 / aula 45: Exercícios de teste de mesa com while

--- nelio-alves/curso_programacao/src/Main.java
+++ nelio-alves/curso_programacao/src/Main.java
@@ -1,18 +1,18 @@
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        /*double preco = 34.5;
-        double desconto;
-        if (preco < 20.0) {
-            desconto = preco * 0.1;
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        int soma = 0;
+        while (x != 0) {
+            soma = soma + x;
+            x = sc.nextInt();
         }
-        else {
-            desconto = preco * 0.05;
-        }*/
 
-        double preco = 34.5;
-        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
-
-        System.out.println(desconto);
+        System.out.println(soma);
+        sc.close();
     }
 }
