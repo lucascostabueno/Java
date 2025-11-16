53261b067906ab88e231f6b8ad3c7733f019c691
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-16 11:40:35 -0300

    secao 6 / aula 50: Estrutura repetitiva para (for)

--- nelio-alves/curso_programacao/src/Main.java
+++ nelio-alves/curso_programacao/src/Main.java
@@ -2,17 +2,26 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
+        int N = sc.nextInt();
 
         int soma = 0;
-        while (x != 0) {
+        for (int i = 0; i <= N; i++) {
+            int x = sc.nextInt();
             soma = soma + x;
-            x = sc.nextInt();
         }
 
         System.out.println(soma);
-        sc.close();
+
+        sc.close();*/
+
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println("Valor de i: " + i);
+        }*/
+
+        for (int i=4; i>=0; i--) {
+            System.out.println("Valor de i: " + i);
+        }
     }
 }
