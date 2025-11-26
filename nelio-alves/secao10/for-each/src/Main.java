2bea4542c4cdcf6ad7427ed28a41dc11589f2631
lucascostabueno <lucasbueno722g@gmail.com>
2025-11-26 11:32:33 -0400

    secao 10 / aula 97: Laço for each

---
+++ nelio-alves/secao10/for-each/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+
+        System.out.println("* for: ");
+        for (int i = 0 ; i < vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+
+        System.out.println("* for each: ");
+        for (String obj: vect) {
+            System.out.println(obj);
+        }
+    }
+}
