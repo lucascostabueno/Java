abaedaf556e40048597c3405a94881461168f436
lucascostabueno <lucasbueno722g@gmail.com>
2025-12-06 10:11:32 -0300

    secao 13 / aula 142: Classes abstratas

--- nelio-alves/secao13/heranca/src/application/Main.java
+++ nelio-alves/secao13/heranca/src/application/Main.java
@@ -4,15 +4,29 @@ import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Account x = new Account (1020, "Alex", 1000.0);
-        Account y = new SavingsAccount (1023, "Maria", 1000.0, 0.01);
+        List<Account> list = new ArrayList<>();
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+        list.add(new SavingsAccount (1001, "Alex", 500.00, 0.01));
+        list.add(new BusinessAccount (1002, "Maria", 1000.0, 400.0));
+        list.add(new SavingsAccount (1004, "Bob", 300.0, 0.01));
+        list.add(new BusinessAccount (1005, "Anna", 500.0, 500.0));
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        double sum = 0.0;
+        for (Account acc : list) {
+            sum += acc.getBalance();
+        }
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for (Account acc : list) {
+            acc.deposit(10.0);
+        }
+        for (Account acc : list) {
+            System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
     }
 }
