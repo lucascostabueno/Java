fa5308f62a12a0c0aa481cf3c2516e0c34ad26ee
lucascostabueno <lucasbueno722g@gmail.com>
2025-12-07 11:07:08 -0300

    secao 14 / aula 150: Pilha de chamadas de métodos (stack trace)

---
+++ nelio-alves/secao14/stack-trace/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        method1();
+
+        System.out.println("End of program");
+    }
+
+    public static void method1() {
+        method2();
+    }
+
+    public static void method2() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+            e.printStackTrace();
+            sc.next();
+        } catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+
+        sc.close();
+    }
+}
