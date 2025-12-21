37154546698206495e92509b09fe67d840849637
lucascostabueno <lucasbueno722g@gmail.com>
2025-12-20 21:29:38 -0300

    secao 21 / aula 268: Nivelamento JPA / Hibernate - PARTE 3

--- nelio-alves/secao21/aulajpamaven/src/main/java/com/lucas/aplicacao/Main.java
+++ nelio-alves/secao21/aulajpamaven/src/main/java/com/lucas/aplicacao/Main.java
@@ -1,15 +1,23 @@
 package com.lucas.aplicacao;
 
 import com.lucas.dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa (1, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa (2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa (3, "Ana Maria", "ana@gmail.com");
+        Pessoa p1 = new Pessoa (null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa (null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa (null, "Ana Maria", "ana@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto!");
     }
 }
